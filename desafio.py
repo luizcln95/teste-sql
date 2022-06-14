7ca71038efd68106d230e62e08d3f398a6d89286
Luiz Leão <luizcln95@gmail.com>
2022-06-14 15:08:49 -0300

    Resolução - Fase 2 do Desafio

--- desafio.py
+++ desafio.py
@@ -12,6 +12,8 @@ Utilizando a tabela de transações, você deve implementar trechos de código
 que respondam as seguintes perguntas:
 '''
 
+cursor.execute(queries.drop_transactions_table)
+cursor.execute(queries.transactions_table)
 
 # 1. Qual é o endereço (carteira) com maior volume de transações enviadas?
 cursor.execute(queries.f1_q1)
@@ -44,3 +46,42 @@ e que seja possível existir saldo negativo).
 '''
 cursor.execute(queries.f1_q5)
 cart_maior_saldo = cursor.fetchone()
+
+
+'''
+Fase 2 – Análise sobre as taxas:
+Utilizando a tabela de transações e a de taxas, você deve implementar trechos de código 
+que respondam as seguintes perguntas:
+'''
+
+cursor.execute(queries.drop_transactions_table)
+cursor.execute(queries.drop_fees_table)
+cursor.execute(queries.transactions_table)
+cursor.execute(queries.fees_table)
+
+
+'''
+1. Considerando que a carteira origem é responsável por pagar as taxas de envio, 
+qual carteira seria responsável pelo maior pagamento de taxas em janeiro de 2021?
+'''
+
+cursor.execute(queries.f2_q1)
+maior_taxa_jan = cursor.fetchone()
+
+
+# 2. E em fevereiro de 2021?
+
+cursor.execute(queries.f2_q2)
+maior_taxa_fev = cursor.fetchone()
+
+
+# 3. Qual é o id da transação com a maior taxa paga?
+
+cursor.execute(queries.f2_q3)
+transacao_maior_taxa = cursor.fetchone()
+
+
+# 4. Qual é a média de taxa paga considerando todas as transações realizadas?
+
+cursor.execute(queries.f2_q4)
+media_taxas = cursor.fetchone()
--- queries.py
+++ queries.py
@@ -1,6 +1,39 @@
+drop_transactions_table = ('''
+            DROP TEMPORARY TABLE IF EXISTS transactions_table;
+            ''')
+
+transactions_table = ('''
+                    CREATE TEMPORARY TABLE transactions_table
+                    SELECT DISTINCT IdTransaction, AddressOrigin, AddressDestination, 
+                    CAST(REPLACE(TotalSent, ',', '') AS DECIMAL(10,2)) AS TotalSent, 
+                    Status, 
+                    CAST(SentDate AS DATETIME) AS SentDate
+                    FROM raw_transactions_table
+                    WHERE TotalSent <> 0.00;
+                    ''')
+
+drop_fees_table = ('''
+            DROP TEMPORARY TABLE IF EXISTS fees_table;
+            ''')
+
+fees_table = ('''
+            CREATE TEMPORARY TABLE fees_table
+            SELECT IdTransaction, AddressOrigin, TotalSent, Status, SentDate, 
+            MONTH(SentDate) AS TransactionMonth, YEAR(SentDate) AS TransactionYear,
+            CASE 
+                WHEN TotalSent > 833000.00 THEN 2.00
+                WHEN TotalSent > 670000.00 THEN 4.00
+                WHEN TotalSent > 500000.00 THEN 5.00
+                WHEN TotalSent > 340000.00 THEN 6.00
+                WHEN TotalSent > 160000.00 THEN 8.00
+                ELSE 10.00 END 
+                AS FeePercentage
+            FROM transactions_table;
+            ''')
+
 f1_q1 = ('''
         SELECT AddressOrigin, SUM(TotalSent) AS TotalSentSum
-        FROM raw_transactions_table
+        FROM transactions_table
         WHERE Status = 'Confirmed'
         GROUP BY AddressOrigin
         ORDER BY TotalSentSum DESC
@@ -9,7 +42,7 @@ f1_q1 = ('''
 
 f1_q2 = ('''
         SELECT DAYOFMONTH(SentDate) as MonthDay, SUM(TotalSent) AS TotalSentSum
-        FROM raw_transactions_table
+        FROM transactions_table
         WHERE Status = 'Confirmed'
         GROUP BY MonthDay
         ORDER BY TotalSentSum DESC
@@ -25,7 +58,7 @@ f1_q3 = ('''
                     WHEN WEEKDAY(SentDate) = 5 THEN 'Saturday'
                     WHEN WEEKDAY(SentDate) = 6 THEN 'Sunday' END
                     AS WeekDay, COUNT(IdTransaction) AS TransactionsCount
-        FROM raw_transactions_table
+        FROM transactions_table
         WHERE Status = 'Confirmed'
         GROUP BY WeekDay
         ORDER BY TransactionsCount DESC
@@ -33,21 +66,56 @@ f1_q3 = ('''
         ''')
 
 f1_q4 = ('''
-        SELECT * FROM raw_transactions_table
+        SELECT * FROM transactions_table
         WHERE AddressOrigin = AddressDestination;
         ''')
 
 f1_q5 = ('''
+        WITH cte AS (SELECT IdTransaction, AddressOrigin, AddressDestination, 
+        CAST(REPLACE(TotalSent, ',', '') AS DECIMAL(10,2)) AS TotalSent, 
+        Status, CAST(SentDate AS DATETIME) AS SentDate
+        FROM raw_transactions_table)
         SELECT AddressOrigin, (TotalReceived - TotalSent) AS Balance
         FROM 
         (SELECT AddressOrigin, sum(TotalSent) AS TotalSent
-        FROM raw_transactions_table
+        FROM cte
         WHERE Status = 'Confirmed'
         GROUP BY AddressOrigin) AS subquery1,
         (SELECT AddressDestination, sum(TotalSent) AS TotalReceived
-        FROM raw_transactions_table
+        FROM cte
         WHERE Status = 'Confirmed'
         GROUP BY AddressDestination) AS subquery2
         WHERE subquery1.AddressOrigin = subquery2.AddressDestination
-        ORDER BY Balance DESC;
+        ORDER BY Balance DESC
+        LIMIT 1;
+        ''')
+
+f2_q1 = ('''
+        SELECT AddressOrigin, SUM(((TotalSent * FeePercentage) / 100)) AS FeeAmount
+        FROM fees_table
+        WHERE TransactionMonth = 1 AND TransactionYear = 2021
+        GROUP BY AddressOrigin
+        ORDER BY FeeAmount DESC
+        LIMIT 1;
+        ''')
+
+f2_q2 = ('''
+        SELECT AddressOrigin, SUM(((TotalSent * FeePercentage) / 100)) AS FeeAmount
+        FROM fees_table
+        WHERE TransactionMonth = 2 AND TransactionYear = 2021
+        GROUP BY AddressOrigin
+        ORDER BY FeeAmount DESC
+        LIMIT 1;
+        ''')
+
+f2_q3 = ('''
+        SELECT IdTransaction, ((TotalSent * FeePercentage) / 100) AS Fee
+        FROM fees_table
+        ORDER BY Fee DESC
+        LIMIT 1;
+        ''')
+
+f2_q4 = ('''
+        SELECT AVG((TotalSent * FeePercentage) / 100) AS AvgFee
+        FROM fees_table;
         ''')
